04f23a4bd1ccc487ed30d4dfa2a611d656852f0e
Abdoul Razak SAWADOGO <abdoulswdg01@gmail.com>
2024-12-18 18:25:07 +0300

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module yeni {
+	requires java.desktop;
+	requires java.sql;
+}
---
+++ src/yeni/Database.java
@@ -0,0 +1,28 @@
+package yeni;
+
+
+	
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+
+	public class Database {
+
+	    private static final String URL = "jdbc:mysql://localhost:3306/deneme3";  // URL de la base de données
+	    private static final String USER = "root";  // Utilisateur MySQL
+	    private static final String PASSWORD = "#";  // Mot de passe (vide par défaut pour root)
+
+	    public static Connection getConnection() throws SQLException {
+	        try {
+	            // Charger le driver JDBC MySQL
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            // Retourner la connexion
+	            return DriverManager.getConnection(URL, USER, PASSWORD);
+	        } catch (ClassNotFoundException e) {
+	            throw new SQLException("JDBC Driver non trouvé", e);
+	        }
+	    }
+	}
+
+
+
